408c242cd0e1d4b5c9374219cc52b6701cfbc0ca
sowjanyahere <nimmalasowjanya@gmail.com>
2024-01-21 15:08:29 +0530

    Added Tittle and functional Components using reactjs

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client" 
 
-const heading = React.createElement("h1",{},"Hello World from react!");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading)
 
-    console.log(heading)
+//React Element
+const Tittle = () => (<h1>Hello World from react World!</h1>);
+
+//React Functional Component
+
+const HeadingComponent = () => {
+   return <div id="contanier">
+        <Tittle />
+        <h1 className="heading">React Functional Component</h1>
+    </div>
+}
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(<HeadingComponent/>)
